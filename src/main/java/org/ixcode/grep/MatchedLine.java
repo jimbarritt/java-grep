7d8ac7c92b25f007478da8e998f6cd27a18eb390
Jim Barritt <jim@planet-ix.com>
2010-05-28 12:23:17 +0100

    Added groups to match result

--- src/main/java/org/ixcode/grep/MatchedLine.java
+++ src/main/java/org/ixcode/grep/MatchedLine.java
@@ -1,13 +1,30 @@
 package org.ixcode.grep;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class MatchedLine {
     private final CharSequence line;
+    private List<String> groups;
 
-    public MatchedLine(CharSequence line) {
-        this.line = line;
+    public MatchedLine(CharSequence line, MatchResult matchResult) {
+        this.line = line;        
+        this.groups = extractGroups(matchResult);
+    }
+
+    private List<String> extractGroups(MatchResult matchResult) {
+        List<String> groups = new ArrayList<String>();
+        for (int iGroup=0;iGroup<matchResult.groupCount();++iGroup) {
+            groups.add(matchResult.group(iGroup));
+        }
+        return groups;
     }
 
     public String lineText() {
         return line.toString();
+    }
+
+    public List<String> groups() {
+        return this.groups;
     }
 }
