02fc6bf5c22bdd4ac7c0dad9803a0e0e462563f3
Jim Barritt <jim@planet-ix.com>
2010-06-01 09:41:49 +0100

    Third step, extracted logic that walks over files and got back to green

--- src/main/java/org/ixcode/grep/scan/FileScanner.java
+++ src/main/java/org/ixcode/grep/scan/FileScanner.java
@@ -3,45 +3,41 @@ package org.ixcode.grep.scan;
 import org.apache.log4j.*;
 
 import java.io.*;
+import java.util.*;
 
 public class FileScanner {
     private static final Logger log = Logger.getLogger(FileScanner.class);
 
-    private static final FileFilter DIRECTORY_FILTER = new DirectoryFilter();
-    private final File rootDir;
+    
+    private final SearchDirectory rootDir;
     private final FilenamePattern filenamePattern;
 
     public FileScanner(SearchDirectory rootDir, FilenamePattern filenamePattern) {
-        this.rootDir = rootDir.asFile();                
+        this.rootDir = rootDir;
         this.filenamePattern = filenamePattern;
     }
 
     public void scan(FileScanningAction fileScanningAction) {
         PatternBasedFilenameFilter filter = new PatternBasedFilenameFilter(filenamePattern);
 
-        log.debug("Scanning files matching [" + filenamePattern + "] in [" + rootDir.getAbsolutePath() + "]");
+        log.debug("Scanning files matching [" + filenamePattern + "] in [" + rootDir + "]");
         scanFiles(rootDir, filter, fileScanningAction);
     }
 
-    private void scanFiles(File parentDir, FilenameFilter filter, FileScanningAction fileScanningAction) {
-        File[] files = parentDir.listFiles(filter);
+    private void scanFiles(SearchDirectory parentDir, FilenameFilter filter, FileScanningAction fileScanningAction) {
+        List<SearchFile> files = parentDir.listFiles(filter);
         if (files != null) {
-            for (File file : files) {
-                fileScanningAction.scanFile(new SearchFile(file));
+            for (SearchFile file : files) {
+                fileScanningAction.scanFile(file);
             }
         }
-        File[] directories = parentDir.listFiles(DIRECTORY_FILTER);
+        List<SearchDirectory> directories = parentDir.listDirectories();
         if (directories != null) {
-            for(File directory : directories) {
+            for(SearchDirectory directory : directories) {
                 scanFiles(directory, filter, fileScanningAction);
             }
         }
     }
 
-    private static class DirectoryFilter implements FileFilter {
-        @Override
-        public boolean accept(File file) {
-            return file.isDirectory();
-        }
-    }
+
 }
--- src/main/java/org/ixcode/grep/scan/SearchDirectory.java
+++ src/main/java/org/ixcode/grep/scan/SearchDirectory.java
@@ -1,15 +1,55 @@
 package org.ixcode.grep.scan;
 
 import java.io.*;
+import java.util.*;
 
 public class SearchDirectory {
+
+    private static final FileFilter DIRECTORY_FILTER = new DirectoryFilter();
     private final File delegate;
 
+    public SearchDirectory(File delegate) {
+        this.delegate = delegate;
+    }
+    
     public SearchDirectory(String directoryPath) {
-        this.delegate = new File(directoryPath);        
+        this(new File(directoryPath));        
     }
 
     public File asFile() {
         return delegate;
+    }
+
+    public String toString() {
+        return delegate.getAbsolutePath();
+    }
+
+    public List<SearchFile> listFiles(FilenameFilter filter) {
+        ArrayList<SearchFile> searchFiles = new ArrayList<SearchFile>();
+
+        File[] files = delegate.listFiles(filter);
+        for (File file : files) {
+            searchFiles.add(new SearchFile(file));
+        }
+
+        return searchFiles;
+    }
+
+    public List<SearchDirectory> listDirectories() {
+        ArrayList<SearchDirectory> directoryArrayList = new ArrayList<SearchDirectory>();
+        File[] directories = delegate.listFiles(DIRECTORY_FILTER);
+        if (directories != null) {
+            for(File directory : directories) {
+                directoryArrayList.add(new SearchDirectory(directory));
+            }
+        }
+        return directoryArrayList;
+    }
+
+    private static class DirectoryFilter implements FileFilter {
+        @Override
+        public boolean accept(File file) {
+            return file.isDirectory();
+        }
     }
 }
