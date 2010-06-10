84a236ab446654637ea14c8db5b887f3bfc356fb
Jim Barritt <jim@planet-ix.com>
2010-06-10 17:16:13 +0100

    Got the full stack working at last!

--- src/test/unit/org/ixcode/grep/GrepIntegrationTest.java
+++ src/test/unit/org/ixcode/grep/GrepIntegrationTest.java
@@ -1,19 +1,45 @@
 package org.ixcode.grep;
 
+import org.apache.log4j.*;
+import org.ixcode.grep.match.*;
 import org.junit.*;
 
 import java.util.*;
 
-import static org.hamcrest.core.Is.*;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class GrepIntegrationTest {
+
+    private static final Logger log = Logger.getLogger(GrepIntegrationTest.class);
 
     @Test    
     public void grepsFiles() {
         List<MatchedFile> matchedFiles = Grep.search("./src/test/resource/testfiles", "*.txt", "todo");
 
-        assertThat(matchedFiles.size(), is(1));                
+        assertThat(matchedFiles.size(), is(7));
+
+        log.info("Results of search:\n");
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        for (MatchedFile matchedFile : matchedFiles) {
+            sb.append(printFile(matchedFile));
+        }
+        log.info(sb.toString());
+    }
+
+    private static String printFile(MatchedFile matchedFile) {
+        StringBuilder sb = new StringBuilder();
+
+        for (MatchedLine matchedLine : matchedFile.matchedLines()) {
+            sb.append(matchedFile.fileName())
+                    .append(":").append(matchedLine.lineNumber())
+                    .append(":").append(matchedLine.start())
+                    .append("\n");
+            sb.append(matchedLine.lineText())
+                    .append("\n");
+        }
+        return sb.toString();
     }
 
 }
